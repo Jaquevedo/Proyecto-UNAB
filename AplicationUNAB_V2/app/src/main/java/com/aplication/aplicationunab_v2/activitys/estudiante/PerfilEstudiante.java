4d4121399a7c6df60a7fde3a42a70d1886741b16
jaquevedo <113221675+jaquevedoUSA@users.noreply.github.com>
2022-12-21 16:01:55 -0500

    Perfil estudiantes y docentes login

--- AplicationUNAB_V2/app/src/main/java/com/aplication/aplicationunab_v2/activitys/estudiante/PerfilEstudiante.java
+++ AplicationUNAB_V2/app/src/main/java/com/aplication/aplicationunab_v2/activitys/estudiante/PerfilEstudiante.java
@@ -1,15 +1,26 @@
 package com.aplication.aplicationunab_v2.activitys.estudiante;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aplication.aplicationunab_v2.R;
+import com.aplication.aplicationunab_v2.activitys.admin.DetallesEstudiantes_Admin;
+import com.aplication.aplicationunab_v2.activitys.admin.ListaEstudiantesAdmin;
 import com.aplication.aplicationunab_v2.activitys.login.LoginActivity;
 import com.aplication.aplicationunab_v2.activitys.password.CambioPassword;
 import com.aplication.aplicationunab_v2.models.Persona;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
@@ -26,17 +37,56 @@ public class PerfilEstudiante extends AppCompatActivity {
     TextView email;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    Persona estudiante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_estudiante);
 
+        estado = findViewById(R.id.txtEstado);
+        documento = findViewById(R.id.docData);
+        nombre = findViewById(R.id.nameData);
+        programa = findViewById(R.id.lunesHorario);
+        email = findViewById(R.id.martesHorario);
+        String idLoginUser = "";
+
+        limpiarTextos();
         Bundle bundle = getIntent().getExtras();
-        if(bundle != null){
-            String idLoginUser = bundle.getString("idEstudiante");
+        if (bundle != null) {
+            idLoginUser = bundle.getString("idEstudiante");
         }
+
+        db.collection("Personas").document(idLoginUser).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                nombre.setText(task.getResult().getString("nombre"));
+                documento.setText(task.getResult().getString("doc"));
+                email.setText(task.getResult().getString("email"));
+                programa.setText(task.getResult().getString("programa"));
+                if (task.getResult().getString("estado").equals("INHABILITADO")){
+                    estado.setText(task.getResult().getString("estado"));
+                    estado.setTextColor(getResources().getColor(R.color.rojo));
+                }else{
+                    estado.setText(task.getResult().getString("estado"));
+                    estado.setTextColor(getResources().getColor(R.color.verde_limon));
+
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(PerfilEstudiante.this, "Hubo un problema con la actualizacion", Toast.LENGTH_SHORT).show();
+                Log.w("Personas", "Error", e);
+            }
+        });
+    }
+
+    private void limpiarTextos() {
+        estado.setText("");
+        documento.setText("");
+        nombre.setText("");
+        programa.setText("");
+        email.setText("");
     }
 
     public void viewOption(View view) {
@@ -63,7 +113,7 @@ public class PerfilEstudiante extends AppCompatActivity {
                 startActivity(intentEstx);
                 break;
 
-            case R.id.btnLogout:
+            case R.id.btnLogoutEst:
                 Intent intentHist = new Intent(PerfilEstudiante.this, LoginActivity.class);
                 startActivity(intentHist);
                 break;
